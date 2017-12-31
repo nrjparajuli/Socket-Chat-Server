130190554fce9c108696116ec09f9f5ce2177dfa
nrjparajuli <nrjparajuli@users.noreply.github.com>
2017-12-30 21:10:56 -0500

    Add files via upload

---
+++ c-s-socket.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <pthread.h>
+
+void error(const char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+
